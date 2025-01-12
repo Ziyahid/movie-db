5a041648a704bb9daaea9d74f948228a86fe04b0
Ziyahid <ziyahid27@gmail.com>
2025-01-12 14:54:45 +0530

    new commit

--- src/routes/AllRoute.jsx
+++ src/routes/AllRoute.jsx
@@ -6,13 +6,13 @@ const AllRoute = () => {
   return (
     <>
     <Routes>
-        <Route path="/" element = {<MovieList title="Your Guide to Great Movies" apiPath="movie/now_playing" />}/>
-        <Route path="movie/popular" element = {<MovieList title="Popular Movies" apiPath="movie/popular" />}/>
-        <Route path="movie/top" element = {<MovieList title="Top Rated Movies" apiPath="movie/top_rated" />}/>
-        <Route path="movie/upcoming" element = {<MovieList title="Upcoming Movies" apiPath="movie/upcoming" />}/>
-        <Route path='movie/:id' element={<MovieDetails/>}/>
-        <Route path='search' element={<Search apiPath="search/movie"/>}/>
-
+        
+        <Route path="movie-db" element = {<MovieList title="Your Guide to Great Movies" apiPath="movie/now_playing" />}/>
+        <Route path="/movie/popular" element = {<MovieList title="Popular Movies" apiPath="movie/popular" />}/>
+        <Route path="/movie/top" element = {<MovieList title="Top Rated Movies" apiPath="movie/top_rated" />}/>
+        <Route path="/movie/upcoming" element = {<MovieList title="Upcoming Movies" apiPath="movie/upcoming" />}/>
+        <Route path='/movie/:id' element={<MovieDetails/>}/>
+        <Route path='/search' element={<Search apiPath="search/movie"/>}/>
         <Route path='*' element={<PageNotFound/>}/>
         
     </Routes>
